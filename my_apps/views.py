4eb2c7f77d3ad0843bc8ff06b502e0011796a959
sontus-tripura <sontustrp.com@gmail.com>
2019-09-03 23:07:46 -0700

    post show and added style on app

--- my_apps/views.py
+++ my_apps/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 
 #from .forms import PostForm
@@ -8,3 +8,9 @@ def post_create(request):
     posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('-publish_date')
     stuff_for_frontend = {'posts': posts}
     return render(request, 'post_create.html', stuff_for_frontend)
+
+#create post detais
+def post_views(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    stuff_for_frontend = { 'posts':post }
+    return render (request, 'post_views.html', stuff_for_frontend)
