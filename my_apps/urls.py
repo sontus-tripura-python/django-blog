35c66eb34effe0bcd127f5954d3561495098175d
sontus-tripura <sontustrp.com@gmail.com>
2019-09-04 08:32:45 -0700

    new post show added

--- my_apps/urls.py
+++ my_apps/urls.py
@@ -3,6 +3,6 @@ from . import views
 urlpatterns = [
      path('', views.post_create, name='post_create'),
      path('post/<int:pk>/', views.post_views, name='post_views'),
-     #path('post/new/', views.post_edit, name='post_edit'),
+     path('post/new/', views.post_new, name='post_new'),
      #path('post/<int:pk>/edit', views.post_edit, name='post_edit'),
     ]
--- my_apps/views.py
+++ my_apps/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from django.utils import timezone
 
-#from .forms import PostForm
+from .forms import PostForm
 from .models import Post
 # Create your views here.
 def post_create(request):
@@ -14,3 +14,15 @@ def post_views(request, pk):
     post = get_object_or_404(Post, pk=pk)
     stuff_for_frontend = { 'posts':post }
     return render (request, 'post_views.html', stuff_for_frontend)
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_views', pk=post.pk)
+    else:
+        form = PostForm()
+        stuff_for_frontend = {'form': form}
+    return render(request, 'post_new.html', stuff_for_frontend)
